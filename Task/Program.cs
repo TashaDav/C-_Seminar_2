a4c82ac0e2bf99304a415b2a6ad5a8dfaf3af04f
davydova <natalya-davydova-86@mail.ru>
2023-04-27 18:36:20 +0300

    Added Task

---
+++ Task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Демонстрация решения
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и 
+// показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+//1. Гернирация случайного числа.
+//2. Найти наибольшую цифру числа.
+//3. Вывод.
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+int a = number / 10;
+int b = number % 10;
+if (a > b)
+{
+    Console.WriteLine($"Our number is {number}. {a} > {b}. Result");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Our number is {number}. {b} > {a}. Result");
+
+}
+else
+{
+    Console.WriteLine($"Our number is {number}. {a} = {b}");
+}
+
