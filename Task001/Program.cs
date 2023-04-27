e4121013bf3462637200784ad5d8aa640901832b
davydova <natalya-davydova-86@mail.ru>
2023-04-27 18:55:25 +0300

    Added Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+int a = number / 100;
+int b = number % 10;
+Console.WriteLine($"{number}");
+Console.WriteLine($"{a}{b}");
